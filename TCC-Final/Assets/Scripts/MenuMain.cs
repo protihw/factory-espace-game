c912ff4b8a9b2945cf8e4c53dfcdfaad324d6c2b
VICENZO <79145837+12334157@users.noreply.github.com>
2023-10-06 08:27:47 -0300

    abuuu

--- TCC-Final/Assets/Scripts/MenuMain.cs
+++ TCC-Final/Assets/Scripts/MenuMain.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuMain : MonoBehaviour
 {
+    public GameObject mainPanel;
+    public GameObject startButton, exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        mainPanel.SetActive(true);
     }
 }
