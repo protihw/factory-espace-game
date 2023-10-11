d45d6c1a657d801757cb472466a81f30505df627
Vicenzo <79145837+vicenzofr@users.noreply.github.com>
2023-10-11 17:07:34 -0300

    inventario mais o menos
    falta ajustar ele e fazer os itens serem coletados e visiveis

---
+++ TCC-Final/Assets/Scripts/InternfaceController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InternfaceController : MonoBehaviour
+{
+
+    public GameObject inventoryPanel;
+    bool invActive;
+
+    void start ()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            invActive = !invActive;
+            inventoryPanel.SetActive(invActive);
+        }
+
+        if (invActive)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ TCC-Final/Assets/Scripts/Objects.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Object", menuName = "Inventory Objects/Create New")]
+public class Objects : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemSprite;
+}
+    
