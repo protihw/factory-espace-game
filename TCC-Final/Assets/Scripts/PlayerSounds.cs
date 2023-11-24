5acbe2a377f2ba5fe1e212165608b6539387015f
proti <bernardo.proti@hotmail.com>
2023-11-24 01:35:29 -0300

    Correndo - sons

--- TCC-Final/Assets/Scripts/PlayerSounds.cs
+++ TCC-Final/Assets/Scripts/PlayerSounds.cs
@@ -8,8 +8,18 @@ public class PlayerSounds : MonoBehaviour
     [SerializeField]
     private AudioClip[] walkingAudioClips;
 
+    [SerializeField]
+    private AudioSource runningAudioSource;
+    [SerializeField]
+    private AudioClip[] runningAudioClips;
+
     public void WalkingMethod()
     {
         walkingAudioSource.PlayOneShot(walkingAudioClips[Random.Range(0, walkingAudioClips.Length)]);
+    }
+
+    public void RunningMethod() 
+    {
+        runningAudioSource.PlayOneShot(runningAudioClips[Random.Range(0, runningAudioClips.Length)]);
     }
 }
